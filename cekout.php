fc2920ed779e9cc3f540aa1fba14fe2e2ae9b9c7
panji343 <39268727+panji343@users.noreply.github.com>
2020-07-24 15:29:16 +0700

    Update cekout.php

--- cekout.php
+++ cekout.php
@@ -209,12 +209,12 @@ $snapToken = Veritrans_Snap::getSnapToken($transaction);
   <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-QBR75dd52EajbqT_"></script>
   <script type="text/javascript">
     document.getElementById('pay-button').onclick = function(){
-      tpl();
       snap.pay('<?=$snapToken?>', {
         onSuccess: function(result){
           document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
         },
         onPending: function(result){
+          tpl();
           document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
         },
         onError: function(result){
